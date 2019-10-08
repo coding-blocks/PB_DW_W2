1a3475126c6e7481e114b4e93010cf785755446a
Prashant SIngh <singh08prashant@gmail.com>
2019-10-08 22:57:46 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+def reverse(s): 
+    return s[::-1] 
+  
+def isPalindrome(s): 
+    # Calling reverse function 
+    rev = reverse(s) 
+  
+    # Checking if both string are equal or not 
+    if (s == rev): 
+        return True
+    return False
+  
+  
+# Driver code 
+s = input("Enter a string: ")
+s=s.lower()
+ans = isPalindrome(s) 
+  
+if ans == 1: 
+    print("Yes") 
+else: 
+    print("No") 
